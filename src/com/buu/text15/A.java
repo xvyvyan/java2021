6a609ea9e30b772631ade8e5cf62923954cc211c
xvyvyan <2534508206@qq.com>
2021-10-17 15:12:57 +0800

    second

--- src/com/buu/text15/A.java
+++ src/com/buu/text15/A.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 public class A {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-
-
-
+        System.out.println("第二");
     }
 }
