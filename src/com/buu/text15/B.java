c87a630ec947e00b94d55de2fb7aaced9fec1ad4
xvyvyan <2534508206@qq.com>
2021-10-17 18:58:13 +0800

    1

---
+++ src/com/buu/text15/B.java
@@ -0,0 +1,12 @@
+package com.buu.text15;
+
+/**
+ * @author wei junbin
+ * @create 2021-10-17 18:54
+ */
+public class B {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+
+}
