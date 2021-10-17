41a23898edc14f8e51d6253a5f09cef137888392
xvyvyan <75837038+xvyvyan@users.noreply.github.com>
2021-10-17 19:24:01 +0800

    Update B.java

--- src/com/buu/text15/B.java
+++ src/com/buu/text15/B.java
@@ -8,6 +8,7 @@ public class B {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
+        System.out.println("git3");
     }
 
 }
