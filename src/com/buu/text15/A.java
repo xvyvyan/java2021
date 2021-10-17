7a71e2b08dd27b572be55f56d6b66b5649d9c9b0
xvyvyan <2534508206@qq.com>
2021-10-17 15:08:47 +0800

    first

---
+++ src/com/buu/text15/A.java
@@ -0,0 +1,16 @@
+package com.buu.text15;
+
+import java.util.ArrayList;
+
+/**
+@author wei junbin
+@create 2021-10-07 17:50
+*/
+public class A {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+
+
+    }
+}
